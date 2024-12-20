cd7c9d583bbd969ff640517671edacc31c44efea
vz-avinokurova <avinokurova@virtuozzo.com>
2024-12-20 17:16:01 +0200

    first commit

---
+++ openstack-creds.sh
@@ -0,0 +1,15 @@
+export OS_PROJECT_DOMAIN_NAME=vhi-s3-ops           # replace "vhi-s3-ops" with your domain name
+export OS_USER_DOMAIN_NAME=vhi-s3-ops              # replace "vhi-s3-ops" with your domain name
+export OS_PROJECT_NAME=student1                    # replace "student1" with your project name
+export OS_USERNAME=user.name                       # replace "user.name" with your user name
+export OS_PASSWORD=**********                      # replace "**********" with password of your user
+export OS_AUTH_URL=https://mycloud.com:5000/v3     # replace "mycloud.com" with the base URL of your cloud panel
+export OS_IDENTITY_API_VERSION=3
+export OS_AUTH_TYPE=password
+export OS_INSECURE=true
+export PYTHONWARNINGS="ignore:Unverified HTTPS request is being made"
+export NOVACLIENT_INSECURE=true
+export NEUTRONCLIENT_INSECURE=true
+export CINDERCLIENT_INSECURE=true
+export OS_PLACEMENT_API_VERSION=1.22
+
